ad2d9b20ceaeda2e4097392f550ffa5f86cdf16e
Leonid Kapitonov <feacur@yandex.ru>
2017-05-16 22:52:41 +0500

    Preparation works, no actual content or final structure: - basic scenes (loader, 3d, UI) - basic prefabs

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+	public string[] sceneNames;
+
+	private IEnumerator Start () {
+		foreach (var sceneName in sceneNames)
+		{
+			var asyncOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+			yield return asyncOperation;
+		}
+	}
+}
