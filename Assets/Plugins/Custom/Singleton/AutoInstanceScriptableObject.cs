a2217ac2ba89575613420f3cbdb30ac244d2dba2
kapitonov <kapitonov@alternativaplatform.com>
2017-05-22 14:53:06 +0500

    Tiny update

--- Assets/Plugins/Custom/Singleton/AutoInstanceScriptableObject.cs
+++ Assets/Plugins/Custom/Singleton/AutoInstanceScriptableObject.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 ///
 /// Provides static access to ScriptableObject of type <typeparam name="T">
 /// Creates an instance if none is present
+/// Use <see cref="destroyed"> to protect against unwanted instantiations
 ///
 /// Intended to be used like
 /// public class ClassName : AutoInstanceScriptableObject<ClassName> { ... }
 ///
 public abstract class AutoInstanceScriptableObject<T> : ScriptableObject where T : AutoInstanceScriptableObject<T> {
+	public static bool destroyed { get; private set; }
+
 	private static T _instance;
 	public static T instance {
 		get {
+			destroyed = false;
 			if (!_instance) {
 				_instance = FindObjectOfType<T>();
 				if (!_instance) {
@@ -25,4 +29,8 @@ public abstract class AutoInstanceScriptableObject<T> : ScriptableObject where T
 	}
 
 	protected virtual void AutoInstanceInit() { }
+
+	protected void OnDestroy() {
+		destroyed = true;
+	}
 }
